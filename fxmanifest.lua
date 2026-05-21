5afb74f2cc908f0fec0f422c700c6d4d9969b9ba
Diamondback Scripts <125171250+DixonHJaynus@users.noreply.github.com>
2026-05-21 16:12:29 -0400

    Fix formatting and update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,6 +1,6 @@
 fx_version 'cerulean'
 game 'rdr3'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* break once RedM ships.'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 
 name 'DB-Boats'
 description 'Boat ownership, storage, and upgrade system for RSG Framework'
